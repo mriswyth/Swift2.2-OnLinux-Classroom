961686fb0564ecd51688ebdd6774c6a77836ae65
Bryan Thompson <ghmris@mristek.com>
2016-04-16 09:36:12 -0400

    Reverse Polish Notation Calculator
    Create the branch for working on rpn_calculator and start the branch
    with the default files for the rpn_calculator package.

---
+++ rpn_calculator/Package.swift
@@ -0,0 +1,12 @@
+/* 
+	Reverse Polish Notation Calculator
+
+	swift-build isn't part of the Swift 2.2 Release so this seems to be 
+	a useless file right now.
+*/
+
+import PackageDescription
+
+let package = Package(
+	name: "rpn_calculator"
+)
